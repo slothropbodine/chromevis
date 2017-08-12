4484a64bf475ca71fb17804544636abda56d3b96
Chris Greenway <chris.greenway@esg-usa.com>
2017-08-12 19:50:54 -0400

    save

--- mycontent.js
+++ mycontent.js
@@ -6,22 +6,11 @@ window.onload = function(event){
 }
 
 function main()  {
-	var hits = $("html").find("div > p.title > a");
-
-	var titles = [];
-    $.each(hits, function( index, value ) {
-		titles.push({'text': this.text});
-    });
-
-	console.log(titles.length);
-	if(titles.length > 25) {
-		chrome.storage.sync.set({'titles': titles}, function() {
-			console.log('Settings saved');
-		});
-	}
-
-	chrome.storage.sync.get(["titles"], function(items){
+	console.log('ff');
+	var barart = $("#playerBarArt");
+	barart.change(function() {
 	});
+	//chrome.storage.sync.set({'titles': titles}, function() { });
 }
 
 
--- popup.js
+++ popup.js
@@ -3,8 +3,6 @@ function onWindowLoad() {
         $('#message').text(items["titles"][1].text);
         $('#message').after('<img id="thing" src="https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/hostedimages/1422163015i/13464993.png" alt="Smiley face">');
 
-	var myElement = document.getElementById("thing");
-
     });
 }
 
